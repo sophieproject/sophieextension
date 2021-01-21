c36711af7671b195a2bd727046c124ca991e8472
William Kenzie <williamkenzie81@gmail.com>
2021-01-21 17:49:46 -0600

    Update

--- scan.js
+++ scan.js
@@ -1,5 +1,27 @@
-var paragraphs = document.getElementsByTagName('p');
-for (var i = 0, l = paragraphs.length; i < l; i++) {
-  console.log(paragraphs[i].text);
-  // scan the file here
+var wordlist = ["pussy", "vagina", "dick", "orgasm", "cum", "sex", "cock", "bareback", "sexy", "cums", "blowjob", "footjob", "handjob", "creampie", "busty"]
+
+function read(text) {
+  for (var i = 0, l = text.length; i < l; i++) {
+    for (var e = 0; e < wordlist.length; e++) {
+      console.log(text[i].innerHTML)
+      if (text[i].innerHTML.toLowerCase().includes(wordlist[e])) {
+        text[i].innerHTML="This content has been blocked.";
+      }
+    }
+  }
 }
+
+var p = document.getElementsByTagName('p');
+read(p);
+var h1 = document.getElementsByTagName('h1');
+read(h1);
+var h2 = document.getElementsByTagName('h2');
+read(h2);
+var h3 = document.getElementsByTagName('h3');
+read(h3);
+var h4 = document.getElementsByTagName('h4');
+read(h4);
+var h5 = document.getElementsByTagName('h5');
+read(h5);
+var a = document.getElementsByTagName('a');
+read(a);
